7ffb2910f134ed0883fd9c0a65747e80539835c5
Kenkibet <85106127+Kenkibet@users.noreply.github.com>
2021-07-26 12:10:23 +0300

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiLink: 'https://api.github.com/search/repositories?q=',
-  apiRepos: '&per_page=10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',
+  apiRepos: '&per_page=10&access_token=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',
   apiUrl: 'https://api.github.com/users/',
   apikey: '?access_token=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',
   apiRepokey: '/repos?access_token=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'
